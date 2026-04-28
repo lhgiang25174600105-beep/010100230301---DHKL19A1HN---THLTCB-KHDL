cea92d718752b085fb41afc7f213debab25bf2cc
lhgiang25174600105-beep <lhgiang.25174600105@sv.uneti.edu.vn>
2026-04-28 12:16:43 +0700

    Delete LAB5/25174600105_Lê Hương Giang directory

--- LAB5/25174600105_Lê Hương Giang/1.py
+++
@@ -1,5 +0,0 @@
-def value(x):
-    return x + 1
-n = int(input("Nhập số nguyên n: "))
-count = value(n)
-print("Số kế tiếp là:", count)
--- LAB5/25174600105_Lê Hương Giang/bai1.py
+++
@@ -1,7 +0,0 @@
-def luy_thua():
-    x = int(input("Nhập số x: "))
-    y = int(input("Nhập số mũ y: "))
-    tso = x**y
-    print(f"Lũy thừa của {x} mũ {y} = {tso}")
-    return
-luy_thua()
